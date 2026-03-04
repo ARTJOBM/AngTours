5b89c7ab1298be516a33a34a48f111a60fafa483
Armen <artgitm@gmail.com>
2026-03-04 18:05:51 +0300

    Menu Bootstrap

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { RegistrationComponent } from './registration/registration.component';
+import {MatTabsModule} from '@angular/material/tabs';
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [AuthorizationComponent, RegistrationComponent, MatTabsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
