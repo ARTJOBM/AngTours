992f1e050293d05b2c8cc8f04f8bd87e803fc85f
Armen <artgitm@gmail.com>
2026-03-18 17:32:05 +0300

    add logout

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { DatePipe } from '@angular/common';
 import { MenuComponent } from "./menu/menu.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,30 +13,30 @@ import { MenuComponent } from "./menu/menu.component";
 export class HeaderComponent implements OnInit {
 
   date = new Date();
-  
+
   private userService = inject(UserService);
+  private router = inject(Router);
 
   get user() {
     return this.userService.getUser();
   }
 
+  menuItems = [
+    { route: '', title: 'Главная' },
+    { route: 'settings', title: 'Настройки' },
+    { route: 'auth', title: 'Авторизация' }
+  ];
 
-
-menuItems = [
-  { route: '', title: 'Главная' },
-  { route: 'settings', title: 'Настройки' },
-  { route: 'auth', title: 'Авторизация' }
-];
-
+  logout() {
+    this.router.navigate(['/auth']);
+  }
 
   ngOnInit(): void {
-
     const raw = localStorage.getItem('user');
     if (raw) {
       this.userService.setUser(JSON.parse(raw));
     }
 
- 
     setInterval(() => {
       this.date = new Date();
     }, 1000);
